0e8b0fed6a0f53e95377648d09793b103272ac85
Thao P Le <tk.le@unimelb.edu.au>
2024-10-22 14:57:52 +1100

    Completing successful runs of simulate_outbreak function

--- scenarios/run_simulator_ABM.py
+++ scenarios/run_simulator_ABM.py
@@ -1,9 +1,9 @@
-
 # to be possibly converted into a test
 
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__),".."))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 import datetime
 import json
@@ -12,21 +12,74 @@ import pickle
 import simulator.simulator as simulator
 
 
-xrange = [150,151]
+xrange = [150, 151]
 yrange = [-30, -29]
 
-set_up_params = {'n': 10, # total number of properties to include
-    'r': 25, # note that this should be in KILOMETERS, the maximum wind dispersal distance
-    'xrange':xrange,
-    'yrange':yrange,
-    'average_property_ha': 500
-
+set_up_params = {
+    "n": 20,  # total number of properties to include
+    "r_wind": 25,  # note that this should be in KILOMETERS, the maximum wind dispersal distance
+    "xrange": xrange,
+    "yrange": yrange,
+    "average_property_ha": 500,  # TODO this isn't exactly the average property size...but it *is* related to it. There may be a minimum size to this...
+    "average_animals_per_ha": 0.2,
+    "movement_frequency": 3,
 }
 
 
-folder_path_main = os.path.join(os.path.dirname(__file__),"outputs","temp")
-if not os.path.exists(folder_path_main ):
-    os.makedirs(folder_path_main )
+folder_path_main = os.path.join(os.path.dirname(__file__), "outputs", "temp")
+if not os.path.exists(folder_path_main):
+    os.makedirs(folder_path_main)
 
-property_setup_info = simulator.property_setup(folder_path_main,set_up_params['n'], set_up_params['r'], set_up_params['average_property_ha'], set_up_params['xrange'],set_up_params['yrange'])
+property_setup_info = simulator.property_setup(folder_path_main, **set_up_params)
 
+# TODO : should consider a namedtuple for unpacking...
+(
+    properties,
+    property_coordinates,
+    adjacency_matrix,
+    neighbour_pairs,
+    neighbourhoods,
+    property_polygons,
+    property_polygons_puffed,
+    property_areas,
+) = property_setup_info
+
+params_low_incubation = {
+    "init_vax_probability": 0,
+    "stop_time": 14,
+    "vax_modifier": 0.4,
+    "beta_wind": 0.1,
+    "beta_animal": 2,
+    "latent_period": 2,
+    "infectious_period": 1,
+    "preclinical_period": 2,
+    "prob_vaccinate": 0.5,
+    "clinical_reporting_threshold": 0.05,
+    "prob_report": 0.5,
+    "movement_probability": 0.1,
+    "movement_prop_animals": 0.2,
+}
+disease = "low_incubation"
+
+params = {**set_up_params, **params_low_incubation}
+
+plotting = True
+unique_output = f'{disease}_{datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")}'
+# make a new folder
+folder_path = os.path.join(folder_path_main, unique_output)
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+file = os.path.join(folder_path, "params.json")
+with open(file, "w") as f:
+    json.dump(params, f)
+
+
+simulator.simulate_outbreak(
+    plotting=plotting,
+    folder_path=folder_path,
+    properties=properties,
+    property_coordinates=property_coordinates,
+    unique_output=unique_output,
+    **params,
+)
