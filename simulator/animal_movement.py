11f6c16c87c789d2ab5e0b5ef000ca3b7c9ed3f1
Thao P Le <tk.le@unimelb.edu.au>
2024-10-23 17:12:24 +1100

    Implemented animal movement recording
    Note that animal_movement was copied from the FMD_modelling module and adapted

---
+++ simulator/animal_movement.py
@@ -0,0 +1,92 @@
+""" Animal movement code
+
+    Adapted from the animal_movement_code.py in the FMD_modelling module
+    (Adapted to add more control over movements and output movements that had occured for contact tracting purposes)
+
+"""
+
+import numpy as np
+from simulator.premises import convert_time_to_date
+
+
+def animal_movement(
+    properties, n, movement_frequency, movement_probability, movement_prop_animals, day
+):
+    added_animals = []
+
+    date = convert_time_to_date(day)
+
+    # TODO
+    movement_record = (
+        []
+    )  # rows of day, moving from property index, to property index, plus a narrative report (locations, number of animals moved)
+    # required for: the narrative, but also if we want to implement certain things based on contact tracing
+
+    # take animals out first, then add them to other properties later (so animals don't move twice in one day)
+    for premise_index in range(len(properties)):
+
+        # if it's a movement day for this property
+        if not (
+            (day - properties[premise_index].movement_start_day) % movement_frequency
+        ):
+
+            # if the property hasn't been culled
+            if not properties[premise_index].culled_status:
+
+                # if there is movement
+                prob_movement = np.random.rand()
+                if prob_movement < movement_probability:
+
+                    # where can the animals moving to
+                    # TODO - to add a limit to how far the animals move (i.e. some 1000 kilometers, for example)
+                    moving_to_premise_indices = []
+                    for i in range(n):
+                        if (
+                            not properties[i].culled_status and i != premise_index
+                        ):  # property hasn't been culled and isn't the moving from property
+                            moving_to_premise_indices.append(i)
+
+                    # if there's somewhere to move the animals
+                    if moving_to_premise_indices:
+
+                        # how many animals moving
+                        property_size = len(properties[premise_index].animals)
+                        number_animals = int(
+                            np.floor(movement_prop_animals * property_size)
+                        )
+
+                        # choose random premise to move to
+                        moving_to_premise_index = np.random.choice(
+                            moving_to_premise_indices
+                        )
+
+                        row = [
+                            day,
+                            premise_index,
+                            moving_to_premise_index,
+                            f"DAY {date} - moved {number_animals} animals from property ID {properties[premise_index].id} to property ID {properties[moving_to_premise_index].id}",
+                        ]  # TODO add more information in the narrative report or row as required
+                        movement_record.append(row)
+
+                        # keeping track of moving the animals
+                        moving_animal_list = []
+                        for _ in range(int(number_animals)):
+                            moving_animal_index = np.random.randint(
+                                0, len(properties[premise_index].animals)
+                            )
+                            moving_animal = properties[premise_index].animals.pop(
+                                moving_animal_index
+                            )
+                            moving_animal_list.append(moving_animal)
+
+                        added_animals.append(
+                            [moving_animal_list, moving_to_premise_index]
+                        )
+
+    # move animals to properties
+    if added_animals:  # as long as there are animals to move
+        for moving_list, moving_index in added_animals:
+            for moving_animal in moving_list:
+                properties[moving_index].animals.append(moving_animal)
+
+    return movement_record
