ac14a7c6b3c76e42aa44109153ca21c8ef7bf060
Thao P Le <tk.le@unimelb.edu.au>
2024-11-14 16:58:27 +1100

    Refactoring: new job management class and job functions

--- simulator/management.py
+++ simulator/management.py
@@ -193,3 +193,267 @@ def testing(properties, property_indices, time, test_sensitivity):
             positive_indices.append(index)
 
     return testing_report, positive_indices
+
+
+class JobManager:
+    jobs_queue = []
+    new_jobs = []
+    local_movement_restrictions = []
+
+    def __init__(
+        self,
+        lab_test_sensitivity,
+        clinical_test_sensitivity,
+        cull_delay=1,
+        contact_tracing_delay=0.5,
+        lab_test_delay=1.5,
+        clinical_delay=0.5,
+    ):
+        self.lab_test_sensitivity = lab_test_sensitivity
+        self.clinical_test_sensitivity = clinical_test_sensitivity
+        self.cull_delay = cull_delay
+        self.contact_tracing_delay = contact_tracing_delay
+        self.lab_test_delay = lab_test_delay
+        self.clinical_delay = clinical_delay
+
+    def add_job_to_queue(self, job):
+        exists = False
+        # TODO if the new job completion time is earlier, then it should be added
+        # could probably also add in checks around whether this premise still exists and still needs to have this job completed or not
+        for j in self.jobs_queue:
+            if (
+                j["status"] == "in progress"
+                and j["type"] == job["type"]
+                and j["property_i"] == job["property_i"]
+            ):
+                exists = True
+                break
+
+        if not exists:
+            self.jobs_queue.append(job)
+
+    def conduct_labtesting(self, properties, job, time):
+        testing_report, positive = test_property(
+            properties,
+            job["property_i"],
+            time,
+            self.lab_test_sensitivity,
+            test_type="lab test",
+        )
+        job["status"] = (
+            "complete"  # mark job as complete, slated for removal from the job queue
+        )
+        return testing_report, positive
+
+    def conduct_clinicalobservation(self, properties, job, time):
+        testing_report, positive = test_property(
+            properties,
+            job["property_i"],
+            time,
+            self.clinical_test_sensitivity,
+            test_type="clinical observation",
+        )
+        job["status"] = (
+            "complete"  # mark job as complete, slated for removal from the job queue
+        )
+        return testing_report, positive
+
+    def decision_to_cull(self, property_i, time):
+        report = f"Property {property_i} has been scheduled for culling\n"
+
+        # add culling job to the jobs_queue, as it has a delay
+        new_job = {
+            "status": "in progress",
+            "day": time + self.cull_delay,
+            "type": jobtype.Cull,
+            "property_i": property_i,
+        }
+        self.new_jobs.append(new_job)
+
+        return report
+
+    def schedule_contract_tracing(self, property_i, time):
+        report = f"Property {property_i} has been scheduled for contact tracing\n"
+
+        new_job = {
+            "status": "in progress",
+            "day": time + self.contact_tracing_delay,
+            "type": jobtype.ContactTracing,
+            "property_i": property_i,
+        }
+
+        self.new_jobs.append(new_job)
+
+        return report
+
+    def schedule_lab_testing(self, property_i, time):
+        new_job = {
+            "status": "in progress",
+            "day": time + self.lab_test_delay,
+            "type": jobtype.LabTesting,
+            "property_i": property_i,
+        }
+        self.new_jobs.append(new_job)
+
+        mini_report = (
+            f"Personnel will be sent to property {property_i} for lab testing\n"
+        )
+        return mini_report
+
+    def schedule_clinical_observation(self, property_i, time):
+        new_job = {
+            "status": "in progress",
+            "day": time + self.clinical_delay,
+            "type": jobtype.ClinicalObservation,
+            "property_i": property_i,
+        }
+        self.new_jobs.append(new_job)
+
+    def run_lab_testing_now(self, properties, job, time):
+        new_report = ""
+        new_testing_reports = ""
+        new_combined_narrative = ""
+
+        testing_report, positive = self.conduct_labtesting(properties, job, time)
+
+        new_testing_reports += testing_report
+        new_combined_narrative += testing_report
+        if positive:
+            premise = properties[job["property_i"]]
+
+            # report property
+            premise_report = premise.report_only(time)
+            new_report += premise_report
+            new_combined_narrative += premise_report
+
+            # schedule culling
+            report = self.decision_to_cull(job["property_i"], time)
+            new_report += report
+            new_combined_narrative += report
+
+            # enact local movement restrictions around this property, just in case
+            self.local_movement_restrictions.append(
+                properties[job["property_i"]].polygon
+            )
+
+            report = self.schedule_contract_tracing(job["property_i"], time)
+            new_combined_narrative += report
+        else:
+            # remove any local movement restrictions
+            self.local_movement_restrictions.remove(
+                properties[job["property_i"]].polygon
+            )
+
+            # may have ongoing surveillance here in the future
+        return new_report, new_testing_reports, new_combined_narrative
+
+    def job_manager(self, time, properties, movement_records):
+        new_report = ""
+        new_testing_reports = ""
+        new_combined_narrative = ""
+        new_contact_tracing_reports = ""
+        total_culled_animals = 0
+        contacts_for_plotting = {}  # from property, to properties
+
+        # add in new jobs; this checks for any repeat jobs and doesn't add repeat jobs
+        for job in self.new_jobs:
+            self.add_job_to_queue(job)
+        self.new_jobs = []
+
+        for job in self.jobs_queue:
+            if job["status"] == "in progress" and job["day"] <= time:
+                # job should now be complete
+                if job["type"] == jobtype.LabTesting:
+                    temp_report, temp_testing_reports, temp_combined_narrative = (
+                        self.run_lab_testing_now(properties, job, time)
+                    )
+                    new_report += temp_report
+                    new_testing_reports += temp_testing_reports
+                    new_combined_narrative += temp_combined_narrative
+
+                elif job["type"] == jobtype.ClinicalObservation:
+                    testing_report, positive = self.conduct_clinicalobservation(
+                        properties, job, time
+                    )
+                    new_testing_reports += testing_report
+                    new_combined_narrative += testing_report
+
+                    if positive:
+                        premise = properties[job["property_i"]]
+
+                        # enact local movement restrictions around this property, just in case
+                        self.local_movement_restrictions.append(
+                            properties[job["property_i"]].polygon
+                        )
+
+                        # schedule contact tracing
+                        report = self.schedule_contract_tracing(job["property_i"], time)
+                        new_combined_narrative += report
+
+                        # schedule lab testing (if not yet done)
+                        report = self.schedule_lab_testing(job["property_i"], time)
+                        new_report += report
+                        new_combined_narrative += report
+                    else:
+                        # remove any local movement restrictions
+                        # technically, should check if it's still under lab testing, which means we might still want some movement restrictions lol...
+                        self.local_movement_restrictions.remove(
+                            properties[job["property_i"]].polygon
+                        )
+
+                        # may have ongoing surveillance here in the future
+                elif job["type"] == jobtype.Cull:
+                    premise = properties[job["property_i"]]
+                    premise_report, culled_animals = premise.cull_only(time)
+                    total_culled_animals += culled_animals
+                    new_report += premise_report
+                    new_combined_narrative += premise_report
+
+                    job["status"] = (
+                        "complete"  # mark job as complete, slated for removal from the job queue
+                    )
+                elif job["type"] == jobtype.ContactTracing:
+                    contact_tracing_report, traced_property_indices = contact_tracing(
+                        properties, job["property_i"], movement_records, time
+                    )
+                    new_contact_tracing_reports += contact_tracing_report
+                    new_combined_narrative += contact_tracing_report
+
+                    contacts_for_plotting[job["property_i"]] = traced_property_indices
+
+                    for t_i in traced_property_indices:
+                        # to be honest, this kind of thing should only be notified after successful adding, not before TODO
+                        mini_report = f"Personnel will be sent to traced property {t_i} for clinical observation and lab testing\n"
+                        new_report += mini_report
+                        new_combined_narrative += mini_report
+
+                        self.schedule_clinical_observation(job["property_i"], time)
+
+                        # schedule lab testing (if not yet done)
+                        # technically this might require a longer delay...
+                        report = self.schedule_lab_testing(job["property_i"], time)
+                        new_report += report
+
+                    job["status"] = (
+                        "complete"  # mark job as complete, slated for removal from the job queue
+                    )
+
+        # clean up job queue
+        self.jobs_queue = [
+            job for job in self.jobs_queue if job["status"] == "in progress"
+        ]
+
+        # add in new jobs; this checks for any repeat jobs and doesn't add repeat jobs
+        for job in self.new_jobs:
+            self.add_job_to_queue(job)
+        self.new_jobs = []
+
+        return (
+            new_report,
+            new_testing_reports,
+            new_combined_narrative,
+            new_contact_tracing_reports,
+            self.local_movement_restrictions,
+            total_culled_animals,
+            contacts_for_plotting,
+        )
