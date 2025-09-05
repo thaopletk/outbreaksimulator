0c67841a8adfe230e7b3f80c279b42d4938dab93
Thao P. Le <tk.le@unimelb.edu.au>
2025-09-05 10:08:02 +1000

    adding basic separated disease parameters for the animals

--- scenarios/v05.py
+++ scenarios/v05.py
@@ -23,7 +23,6 @@ import simulator.spatial_functions as spatial_functions
 import simulator.spatial_setup as spatial_setup
 
 folder_path_main = os.path.join(os.path.dirname(__file__), "v05")
-folder_path_seed = os.path.join(folder_path_main, "01_seed")
 
 # make main folder if it doesn't exist
 if not os.path.exists(folder_path_main):
@@ -120,3 +119,57 @@ else:
     # load properties
     with open(properties_filename, "rb") as file:
         properties = pickle.load(file)
+
+
+# limits for the figures
+xlims = [
+    round(spatial_only_parameters["xrange"][0], 2) - 0.005,
+    round(spatial_only_parameters["xrange"][1], 2) + 0.005,
+]
+ylims = [
+    round(spatial_only_parameters["yrange"][0], 1) - 0.05,
+    round(spatial_only_parameters["yrange"][1], 1) + 0.05,
+]
+
+
+# plot the neighbours (not wind-neighbours)
+if not os.path.exists(os.path.join(folder_path_main, "map_underlying_neighbours.png")):
+    output.plot_map(
+        properties,
+        time=0,
+        xlims=xlims,
+        ylims=ylims,
+        folder_path=folder_path_main,
+        real_situation=True,
+        controlzone=None,
+        infectionpoly=False,
+        contacts_for_plotting={},
+        show_movement_neighbours=True,
+    )
+
+
+# plot the animal density
+if not os.path.exists(os.path.join(folder_path_main, "animal_density.png")):
+    output.plot_animal_density(properties, xlims, ylims, folder_path=folder_path_main)
+
+
+# seed infection
+time = 0
+
+folder_path_seed = os.path.join(folder_path_main, "01_seed")
+if not os.path.exists(folder_path_seed):
+    os.makedirs(folder_path_seed)
+
+# parameters
+with open(os.path.join(folder_path_main, "disease_parameters.json"), "r") as file:
+    disease_parameters = json.load(file)
+
+properties_seeded_filename = os.path.join(folder_path_seed, "properties_0")
+
+northVICx = [141, 147]
+northVICy = [-37, -34]
+
+
+# area for first report Victoria
+reportingregion_x = [141, 150]
+reportingregion_y = [-40, -34]
